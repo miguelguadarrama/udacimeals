de162512ec2b274301820b3f109e9f86d25ca916
Miguel Guadarrama <mguadarrama@gcfund.org>
2018-01-02 23:06:30 +0900

    initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux'
+import calendar from './reducers'
+
+const store = createStore(calendar, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+console.log(store.getState());
+
+ReactDOM.render(<App store={store} />, document.getElementById('root'));
+registerServiceWorker();
